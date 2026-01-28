0c34f8cf7c0b271c68e4f8a0825f2f2aac8273ee
lshu <le7yi_ss@hotmail.com>
2026-01-28 22:59:03 +0800

    update

--- ego/wallpaper/renderers.py
+++ ego/wallpaper/renderers.py
@@ -1,8 +1,9 @@
 # your_app/renderers.py
-from rest_framework.renderers import JSONRenderer
-from rest_framework import status
-
 import time
+
+from rest_framework import status
+from rest_framework.renderers import JSONRenderer
+
 
 class CustomJSONRenderer(JSONRenderer):
     def render(self, data, accepted_media_type=None, renderer_context=None):
@@ -13,7 +14,7 @@ class CustomJSONRenderer(JSONRenderer):
 
         # 获取请求开始时间（需通过中间件或视图记录）, 每个接口增加cost_time耗时字段
         request = renderer_context["request"]
-        if hasattr(request, 'start_time'):
+        if hasattr(request, "start_time"):
             duration = time.time() - request.start_time
         else:
             duration = None
@@ -23,9 +24,9 @@ class CustomJSONRenderer(JSONRenderer):
             formatted_data = {
                 "code": code,
                 "message": message,
-                "pagination": data["pagination"],    # 分页信息
-                "data": data["data"],              # 分页数据
-                # "data": data["data"] if code == status.HTTP_200_OK else [],              # 如果报错返回空列表
+                "pagination": data["pagination"],  # 分页信息
+                "data": data["data"],  # 分页数据
+                # "data": data["data"] if code == status.HTTP_200_OK else [],  # 如果报错返回空列表
                 "duration": f"{duration:.2f}s",
             }
         else:
@@ -42,7 +43,7 @@ class CustomJSONRenderer(JSONRenderer):
     def _get_error_message(self, data, response):
         """提取错误信息"""
         if isinstance(data, dict):
-            return data.get("detail", response.status_text)
+            return data.get("detail") or data.get("error") or response.status_text
         elif isinstance(data, list):
             return data[0] if len(data) > 0 else response.status_text
         else:
